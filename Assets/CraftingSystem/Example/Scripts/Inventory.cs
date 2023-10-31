192c84485d937eea36662bf7bbcff88e49687385
5900w <chaewan2002@gmail.com>
2023-11-01 03:08:59 +0900

    Update Inventory.cs

--- Assets/CraftingSystem/Example/Scripts/Inventory.cs
+++ Assets/CraftingSystem/Example/Scripts/Inventory.cs
@@ -12,8 +12,6 @@ public class Inventory : MonoBehaviour
 	}
 	#endregion
 
-	//List<ItemSlot> itemSlots = new List<ItemSlot>();
-
     [SerializeField]
     GameObject inventoryPanel;
 
@@ -21,14 +19,6 @@ public class Inventory : MonoBehaviour
 	private TMPro.TextMeshProUGUI descriptionText;
 	[SerializeField]
 	private TMPro.TextMeshProUGUI nameText;
-
-	void Start()
-    {
-        ////Read all itemSlots as children of inventory panel
-        //itemSlots = new List<ItemSlot>(
-        //    inventoryPanel.transform.GetComponentsInChildren<ItemSlot>()
-        //    );
-    }
 
     public void DisplayMessage(Item item = null)
     {
