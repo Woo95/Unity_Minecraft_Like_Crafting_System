d0ab8eaceb3b8efe00eeb393cb8897e5f8c7143c
5900w <chaewan2002@gmail.com>
2023-11-01 04:15:40 +0900

    Update Recipe.cs

--- Assets/CraftingSystem/Example/Scripts/Recipe.cs
+++ Assets/CraftingSystem/Example/Scripts/Recipe.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 //Attribute which allows right click->Create
 [CreateAssetMenu(fileName = "New Recipe", menuName = "Items/New Recipe")]
-public class Recipe : ScriptableObject					//Extending SO allows us to have an object which exists in the project, not in the scene
+public class Recipe : ScriptableObject
 {
 	public Item[] input = new Item[9];	// 9 = 3x3 crafting input pannel
 	public Item output;
